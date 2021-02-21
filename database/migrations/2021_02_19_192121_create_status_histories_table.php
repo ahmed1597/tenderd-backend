c8c8ccb8ad4a8fbc05eb6cb2d8b6b6efda568e44
ahmed mahmoud <ahmed.mahmoud1597@gmail.com>
2021-02-21 05:22:56 +0400

    added migrations for 5 entities

---
+++ database/migrations/2021_02_19_192121_create_status_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status_histories', function (Blueprint $table) {
+            $table->id();
+            $table->integer("ticket_id");
+            $table->string("from_status");
+            $table->string("to_status");
+            $table->longText("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status_histories');
+    }
+}
